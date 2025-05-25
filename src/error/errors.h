d72ee1e3987c3d48512c87826b3387ae5f17d599
akovtune <akovtune@student.codam.nl>
2025-05-25 15:12:18 +0200

    Updated errors.h Now errors.h includes status_codes.h, so it has all of the possible status codes, which makes sense to me Added new status code INVALID_VALUE

--- src/error/errors.h
+++ src/error/errors.h
@@ -6,20 +6,22 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 12:55:22 by akovtune          #+#    #+#             */
-/*   Updated: 2025/05/17 16:37:04 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/05/25 15:06:02 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERRORS_H
 # define ERRORS_H
 
-# include "ft_string.h"
 # include <MLX42/MLX42.h>
 # include <unistd.h>
+# include "status_codes.h"
+# include "ft_string.h"
 
 # define MLX_ERR 10
 # define MALLOC_FAIL_ERR 11
 # define EMPTY_PTR_ERR 12
+# define INVALID_VALUE 13
 
 const char	*get_err_msg(int err_code);
 void		print_err_msg(const char *message);
