07b0b654ada482d3e9df64411f02ad8441e2090b
akovtune <akovtune@student.codam.nl>
2025-05-26 17:26:48 +0200

    Updated player_builder.h Made the build_player() function accessible by adding it to the header file

--- src/builders/player_builder/player_builder.h
+++ src/builders/player_builder/player_builder.h
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 15:00:03 by akovtune          #+#    #+#             */
-/*   Updated: 2025/05/25 15:01:12 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/05/25 15:45:37 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 # include "player.h"
 # include "errors.h"
+
+int	build_player(t_player *player);
 
 #endif
