4fb23f2984526813a895664c95abfcc56c5724bc
akovtune <akovtune@student.codam.nl>
2025-05-26 17:33:07 +0200

    Updated camera.h
    Added new fields:
    - fov
    - rays_count
    - position
    - angle

--- src/model/core/camera/camera.c
+++ src/model/core/camera/camera.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   camera.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/26 15:53:07 by akovtune          #+#    #+#             */
+/*   Updated: 2025/05/26 17:31:05 by akovtune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "camera/camera.h"
 
 t_camera	*init_camera(void)
@@ -7,7 +19,10 @@ t_camera	*init_camera(void)
 	camera = (t_camera *)malloc(sizeof(t_camera));
 	if (!camera)
 		return (NULL);
+	camera->angle = 0;
 	camera->rays = NULL;
+	camera->rays_count = 0;
+	camera->fov = 0;
 	return (camera);
 }
 
