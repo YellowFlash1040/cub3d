311dfb2b78a0a8e88d5d32d43da537605f330bae
akovtune <akovtune@student.codam.nl>
2025-05-26 18:55:21 +0200

    Updated movements.h and movements.c
    Removed unnecessary status code return
    and switch return data type from int to void

--- src/logic/calculations/movements/movements.c
+++ src/logic/calculations/movements/movements.c
@@ -6,40 +6,36 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 16:55:25 by akovtune          #+#    #+#             */
-/*   Updated: 2025/05/26 17:47:41 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/05/26 18:55:01 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "movements.h"
 
-int	move_player_forward(t_player *player)
+void	move_player_forward(t_player *player)
 {
 	player->position.x += player->direction.x * STEP;
 	player->position.y += player->direction.y * STEP;
 	player->camera->position = player->position;
-	return (SUCCESS);
 }
 
-int	move_player_back(t_player *player)
+void	move_player_back(t_player *player)
 {
 	player->position.x -= player->direction.x * STEP;
 	player->position.y -= player->direction.y * STEP;
 	player->camera->position = player->position;
-	return (SUCCESS);
 }
 
-int	rotate_player_left(t_player *player)
+void	rotate_player_left(t_player *player)
 {
 	player->camera->angle += ROTATION_ANGLE;
 	normalize_angle(&player->camera->angle);
 	player->direction = get_unit_vector(player->camera->angle);
-	return (SUCCESS);
 }
 
-int	rotate_player_right(t_player *player)
+void	rotate_player_right(t_player *player)
 {
 	player->camera->angle -= ROTATION_ANGLE;
 	normalize_angle(&player->camera->angle);
 	player->direction = get_unit_vector(player->camera->angle);
-	return (SUCCESS);
 }
--- src/logic/calculations/movements/movements.h
+++ src/logic/calculations/movements/movements.h
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 16:55:30 by akovtune          #+#    #+#             */
-/*   Updated: 2025/05/26 15:43:13 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/05/26 18:37:27 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@
 # define ROTATION_ANGLE 0.05
 # define STEP 4
 
-int	move_player_forward(t_player *player);
-int	move_player_back(t_player *player);
-int	rotate_player_left(t_player *player);
-int	rotate_player_right(t_player *player);
+void	move_player_forward(t_player *player);
+void	move_player_back(t_player *player);
+void	rotate_player_left(t_player *player);
+void	rotate_player_right(t_player *player);
 
 #endif
