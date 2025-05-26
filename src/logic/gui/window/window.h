7508313d28bd96957d49c1b48f3057334f663f5b
akovtune <akovtune@student.codam.nl>
2025-05-26 17:51:46 +0200

    Updated window.h
    Replaced #include "status_codes.h" with "errors.h"
    Moved WINDOW_WIDTH and WINDOW_HEIGHT definitions into settings.h
    Added #include "settings.h"

--- src/logic/gui/window/window.h
+++ src/logic/gui/window/window.h
@@ -6,18 +6,16 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 17:34:50 by akovtune          #+#    #+#             */
-/*   Updated: 2025/05/16 16:57:55 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/05/26 15:42:47 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WINDOW_H
 # define WINDOW_H
 
-# include "status_codes.h"
 # include <MLX42/MLX42.h>
-
-# define WINDOW_WIDTH 1920
-# define WINDOW_HEIGHT 1080
+# include "settings.h"
+# include "errors.h"
 
 int		connect_to_display(mlx_t **mlx_out);
 void	keep_app_running(mlx_t *mlx);
