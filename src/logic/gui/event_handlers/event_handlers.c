05f87fbc29bdb95421b5ec42260386063084c1a2
akovtune <akovtune@student.codam.nl>
2025-05-28 15:53:37 +0200

    Improved readability inside event_handlers.c by extracting some of the code into separate functions

--- src/logic/gui/event_handlers/event_handlers.c
+++ src/logic/gui/event_handlers/event_handlers.c
@@ -6,11 +6,14 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 16:18:46 by akovtune          #+#    #+#             */
-/*   Updated: 2025/05/28 14:41:33 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/05/28 15:41:15 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "event_handlers.h"
+
+static void	handle_movement(mlx_t *mlx, mlx_key_data_t keydata, t_game *game);
+static void	toggle_minimap(t_settings *settings);
 
 void	subscribe_to_keyboard_events(t_app *app)
 {
@@ -22,22 +25,29 @@ void	handle_key_pressed(mlx_key_data_t keydata, void *param)
 	t_app	*app;
 
 	app = (t_app *)param;
-	if (keydata.key == KEY_UP
-		&& (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
-		move_player_forward(app->game->map, app->game->player);
-	else if (keydata.key == KEY_DOWN
-		&& (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
-		move_player_back(app->game->map, app->game->player);
-	else if (keydata.key == KEY_LEFT
-		&& (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
-		rotate_player_left(app->game->player);
-	else if (keydata.key == KEY_RIGHT
-		&& (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
-		rotate_player_right(app->game->player);
-	else if (keydata.key == KEY_CLOSE_WINDOW
-		&& keydata.action == MLX_PRESS)
+	if (is_movement_key(keydata.key))
+		handle_movement(app->mlx, keydata, app->game);
+	else if (keydata.key == KEY_CLOSE_WINDOW && keydata.action == MLX_PRESS)
 		mlx_close_window(app->mlx);
-	else if (keydata.key == KEY_TOGGLE_MINIMAP
-		&& keydata.action == MLX_PRESS)
-		app->settings->is_minimap_visible = !app->settings->is_minimap_visible;
+	else if (keydata.key == KEY_TOGGLE_MINIMAP && keydata.action == MLX_PRESS)
+		toggle_minimap(app->settings);
+}
+
+static void	handle_movement(mlx_t *mlx, mlx_key_data_t keydata, t_game *game)
+{
+	if (!(keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
+		return ;
+	if (keydata.key == KEY_UP)
+		move_player_forward(game->map, game->player, mlx->delta_time);
+	else if (keydata.key == KEY_DOWN)
+		move_player_back(game->map, game->player, mlx->delta_time);
+	else if (keydata.key == KEY_LEFT)
+		rotate_player_left(game->player);
+	else if (keydata.key == KEY_RIGHT)
+		rotate_player_right(game->player);
+}
+
+static void	toggle_minimap(t_settings *settings)
+{
+	settings->is_minimap_visible = !settings->is_minimap_visible;
 }
