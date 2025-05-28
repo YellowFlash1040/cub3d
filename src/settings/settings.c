334044b5f913df2e8f619b15728a6c51ca766173
akovtune <akovtune@student.codam.nl>
2025-05-28 18:11:33 +0200

    Updated "settings" class
    Added next fields:
    - ceiling
    - floor
    - north_wall
    - south_wall
    - west_wall
    - east_wall
    
    Also updated constructor and destructor

--- src/settings/settings.c
+++ src/settings/settings.c
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 15:55:49 by akovtune          #+#    #+#             */
-/*   Updated: 2025/05/25 15:56:22 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/05/28 18:08:17 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@ t_settings	*init_settings(void)
 	if (!settings)
 		return (NULL);
 	settings->is_minimap_visible = true;
+	settings->ceiling = NULL;
+	settings->floor = NULL;
+	settings->north_wall = NULL;
+	settings->south_wall = NULL;
+	settings->west_wall = NULL;
+	settings->east_wall = NULL;
 	return (settings);
 }
 
@@ -30,6 +36,18 @@ void	destroy_settings(t_settings **settings_ref)
 	if (!settings_ref || !*settings_ref)
 		return ;
 	settings = *settings_ref;
+	if (settings->ceiling)
+		destroy_surface(&settings->ceiling);
+	if (settings->floor)
+		destroy_surface(&settings->floor);
+	if (settings->north_wall)
+		destroy_wall(&settings->north_wall);
+	if (settings->south_wall)
+		destroy_wall(&settings->south_wall);
+	if (settings->west_wall)
+		destroy_wall(&settings->west_wall);
+	if (settings->east_wall)
+		destroy_wall(&settings->east_wall);
 	free(settings);
 	*settings_ref = NULL;
 }
--- src/settings/settings.h
+++ src/settings/settings.h
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 15:55:51 by akovtune          #+#    #+#             */
-/*   Updated: 2025/05/28 14:01:02 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/05/28 18:03:07 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,20 @@
 # include "window_settings.h"
 # include "camera_settings.h"
 # include "world_settings.h"
+# include "surface.h"
+# include "wall.h"
 
 # define SETTINGS_INIT_ERR 1
 
 typedef struct settings
 {
-	bool	is_minimap_visible;
+	bool		is_minimap_visible;
+	t_surface	*ceiling;
+	t_surface	*floor;
+	t_wall		*north_wall;
+	t_wall		*south_wall;
+	t_wall		*west_wall;
+	t_wall		*east_wall;
 }	t_settings;
 
 t_settings	*init_settings(void);
