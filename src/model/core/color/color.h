bfd36f6facb2216002e46649d8a50e5cb2f2bb22
akovtune <akovtune@student.codam.nl>
2025-05-30 18:11:35 +0200

    Updated "color" and "channel" type definitions inside color.h

--- src/model/core/color/color.h
+++ src/model/core/color/color.h
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 15:41:43 by akovtune          #+#    #+#             */
-/*   Updated: 2025/05/16 15:55:11 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/05/29 18:33:52 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 # include <stdlib.h>
 
-typedef unsigned char	t_channel;
-typedef __int32_t		t_color;
+typedef __uint8_t	t_channel;
+typedef __uint32_t		t_color;
 
 t_color	get_color(t_channel r, t_channel g, t_channel b, t_channel a);
 
