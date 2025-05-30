21484a6cd44ebf79bdc2895e4261680412dec801
akovtune <akovtune@student.codam.nl>
2025-05-30 18:59:40 +0200

    Updated "wall" class using new "texture" class

--- src/model/gui/textures/wall/wall.h
+++ src/model/gui/textures/wall/wall.h
@@ -6,23 +6,23 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 15:29:33 by akovtune          #+#    #+#             */
-/*   Updated: 2025/05/29 16:40:22 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/05/30 18:48:15 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WALL_H
 # define WALL_H
 
-# include <MLX42/MLX42.h>
 # include <stdlib.h>
 # include "direction.h"
+# include "texture.h"
 
 # define WALL_INIT_ERR 1
 
 typedef struct wall
 {
 	t_direction		direction;
-	mlx_texture_t	*texture;
+	t_texture		*texture;
 }	t_wall;
 
 t_wall	*init_wall(void);
