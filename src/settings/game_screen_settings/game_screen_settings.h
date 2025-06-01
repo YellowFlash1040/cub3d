60f01672b2ecac4450f70644a3c25033241f5d0f
akovtune <akovtune@student.codam.nl>
2025-06-01 18:56:51 +0200

    Created game_screen_settings inside settings/ folder with the next variables (#define): - GAME_SCREEN_WIDTH - GAME_SCREEN_HEIGHT - GAME_SCREEN_X_POSITION - GAME_SCREEN_Y_POSITION
    to control the size of the scene (game screen)

---
+++ src/settings/game_screen_settings/game_screen_settings.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_screen_settings.h                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/01 18:39:16 by akovtune          #+#    #+#             */
+/*   Updated: 2025/06/01 18:52:02 by akovtune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GAME_SCREEN_SETTINGS_H
+# define GAME_SCREEN_SETTINGS_H
+
+# include "window_settings.h"
+
+# define GAME_SCREEN_WIDTH 1200
+# define GAME_SCREEN_HEIGHT WINDOW_HEIGHT
+
+// # define GAME_SCREEN_WIDTH 500
+// # define GAME_SCREEN_HEIGHT 500
+
+# define GAME_SCREEN_X_POSITION 700
+# define GAME_SCREEN_Y_POSITION 0
+
+#endif
