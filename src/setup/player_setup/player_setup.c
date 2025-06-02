af2840dfe42133ac7739799a1f41376d4506aac5
YellowFlash1040 <andrejkovtunec@gmail.com>
2025-06-02 21:13:59 +0200

    Fixed norminette inside player_setup.c

--- src/setup/player_setup/player_setup.c
+++ src/setup/player_setup/player_setup.c
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 16:56:47 by akovtune          #+#    #+#             */
-/*   Updated: 2025/06/02 14:54:29 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/02 21:02:17 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@ void	setup_player(t_player *player, t_settings *settings)
 {
 	double	perpendicular_angle;
 
-	player->position = settings->player_position;
-	player->vertical_direction = get_unit_vector(settings->camera_angle);
-
 	perpendicular_angle = get_perpendicular_angle(settings->camera_angle);
 	normalize_angle(&perpendicular_angle);
+	player->position = settings->player_position;
+	player->vertical_direction = get_unit_vector(settings->camera_angle);
 	player->horizontal_direction = get_unit_vector(perpendicular_angle);
 }
