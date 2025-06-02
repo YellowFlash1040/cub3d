6af85ba5638e135eb36d56bb53ef2aad90a3ca01
YellowFlash1040 <andrejkovtunec@gmail.com>
2025-06-02 12:40:12 +0200

    Updated camera_settings.h and added some more comments

--- src/settings/camera_settings/camera_settings.h
+++ src/settings/camera_settings/camera_settings.h
@@ -6,14 +6,15 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 13:47:49 by akovtune          #+#    #+#             */
-/*   Updated: 2025/06/01 18:52:28 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/02 12:39:47 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CAMERA_SETTINGS_H
 # define CAMERA_SETTINGS_H
 
-# define RAYS_COUNT 120
+// # define RAYS_COUNT 120
+# define RAYS_COUNT 100
 # define CAMERA_FIELD_OF_VIEW 60
 // # define ROTATION_ANGLE 0.05
 # define ROTATION_SPEED 2
@@ -25,9 +26,14 @@
 /*
 GAME SCREEN SIZE PROBLEM
 
+Thought 1:
 In order to understand how wide a line on the screen needs to be
 you need to divide window width by amount of rays
 line_width = WINDOW_WIDTH / RAYS_COUNT
+
+Thought 2:
+In order for your game screen (scene) to look nice,
+the GAME_SCREEN_WIDTH has to be divisible by RAYS_COUNT without a remainder
 */
 
 /*
@@ -35,6 +41,6 @@ TEXTURES PROBLEM
 
 Some of the textures (north side and east side) are being mirrored
 
-I think (because I remember some of the information from 3dsage's video)
-that it has something to do with x and y coordinates
+It has something to do with the side a ray is facing
+and with x and y coordinates, but I'm not quite sure why exactly
 */
