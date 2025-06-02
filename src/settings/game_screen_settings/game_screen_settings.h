e5bc552746b4d0e71459fd7ea0c22aa466a32a9b
YellowFlash1040 <andrejkovtunec@gmail.com>
2025-06-02 12:44:44 +0200

    Updated game_screen_settings.h

--- src/settings/game_screen_settings/game_screen_settings.h
+++ src/settings/game_screen_settings/game_screen_settings.h
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 18:39:16 by akovtune          #+#    #+#             */
-/*   Updated: 2025/06/01 18:52:02 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/02 12:44:22 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,18 @@
 
 # include "window_settings.h"
 
-# define GAME_SCREEN_WIDTH 1200
+// Codam PC setup
+// # define GAME_SCREEN_WIDTH 1200
+// # define GAME_SCREEN_HEIGHT WINDOW_HEIGHT
+
+// # define GAME_SCREEN_X_POSITION 700
+// # define GAME_SCREEN_Y_POSITION 0
+
+// My home PC setup
+# define GAME_SCREEN_WIDTH 600
 # define GAME_SCREEN_HEIGHT WINDOW_HEIGHT
 
-// # define GAME_SCREEN_WIDTH 500
-// # define GAME_SCREEN_HEIGHT 500
-
-# define GAME_SCREEN_X_POSITION 700
+# define GAME_SCREEN_X_POSITION 300
 # define GAME_SCREEN_Y_POSITION 0
 
 #endif
