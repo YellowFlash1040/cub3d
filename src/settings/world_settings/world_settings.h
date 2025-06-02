d6248c236ddb8ccd1deb1e602d25116d1fa71076
YellowFlash1040 <andrejkovtunec@gmail.com>
2025-06-02 12:45:50 +0200

    Updated world_settings.h

--- src/settings/world_settings/world_settings.h
+++ src/settings/world_settings/world_settings.h
@@ -6,13 +6,17 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 13:48:41 by akovtune          #+#    #+#             */
-/*   Updated: 2025/05/28 13:56:42 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/02 12:45:34 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WORLD_SETTINGS_H
 # define WORLD_SETTINGS_H
 
-# define CELL_SIZE 64
+// Codam PC setup
+// # define CELL_SIZE 64
+
+// My home PC setup
+# define CELL_SIZE 32
 
 #endif
