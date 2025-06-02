39cea3e6332e0de1fcc32b0656191066dcad43d4
YellowFlash1040 <andrejkovtunec@gmail.com>
2025-06-02 12:42:12 +0200

    Updated window_settings.h

--- src/settings/window_settings/window_settings.h
+++ src/settings/window_settings/window_settings.h
@@ -6,15 +6,19 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 13:47:19 by akovtune          #+#    #+#             */
-/*   Updated: 2025/06/01 18:39:26 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/02 12:41:04 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WINDOW_SETTINGS_H
 # define WINDOW_SETTINGS_H
 
-# define WINDOW_WIDTH 1920
-// # define WINDOW_WIDTH 2400
-# define WINDOW_HEIGHT 1080
+// Codam PC setup
+// # define WINDOW_WIDTH 1920
+// # define WINDOW_HEIGHT 1080
+
+// My home PC setup
+# define WINDOW_WIDTH 960
+# define WINDOW_HEIGHT 540
 
 #endif
