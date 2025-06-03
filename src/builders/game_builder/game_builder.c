496f5a4b24ea334b56e3eafb6888e2cf4fabc2ca
akovtune <akovtune@student.codam.nl>
2025-06-03 18:40:10 +0200

    Updated game_builder

--- src/builders/game_builder/game_builder.c
+++ src/builders/game_builder/game_builder.c
@@ -6,11 +6,13 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 14:44:44 by akovtune          #+#    #+#             */
-/*   Updated: 2025/05/25 15:05:08 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/03 18:03:26 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game_builder.h"
+
+static void	destroy_temp(t_game *game);
 
 int	build_game(t_game *game)
 {
@@ -18,9 +20,19 @@ int	build_game(t_game *game)
 		return (EMPTY_PTR_ERR);
 	game->map = init_map();
 	if (!game->map)
-		return (MALLOC_FAIL_ERR);
+		return (destroy_temp(game), MALLOC_FAIL_ERR);
 	game->player = init_player();
 	if (!game->player)
-		return (destroy_map(&game->map), MALLOC_FAIL_ERR);
+		return (destroy_temp(game), MALLOC_FAIL_ERR);
 	return (SUCCESS);
+}
+
+static void	destroy_temp(t_game *game)
+{
+	if (!game)
+		return ;
+	if (game->map)
+		destroy_map(&game->map);
+	if (game->player)
+		destroy_player(&game->player);
 }
