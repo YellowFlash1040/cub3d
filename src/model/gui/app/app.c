b101586e7382b52daee83ffc2e227a1061094048
akovtune <akovtune@student.codam.nl>
2025-06-03 18:43:22 +0200

    Updated destructor of the "app" class

--- src/model/gui/app/app.c
+++ src/model/gui/app/app.c
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 13:56:51 by akovtune          #+#    #+#             */
-/*   Updated: 2025/05/29 17:08:53 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/03 17:42:57 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,8 @@ void	destroy_app(t_app **app_ref)
 		destroy_textures(&app->textures);
 	if (app->canvas)
 		destroy_canvas(&app->canvas);
-	mlx_terminate(app->mlx);
+	if (app->mlx)
+		mlx_terminate(app->mlx);
 	free(app);
 	*app_ref = NULL;
 }
