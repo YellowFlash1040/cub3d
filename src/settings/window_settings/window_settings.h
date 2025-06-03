4030779900bcbb4143d40f5d7cd3d617156e8801
akovtune <akovtune@student.codam.nl>
2025-06-03 18:45:30 +0200

    Created WINDOW_TITLE variable (#define) inside window_settings.h

--- src/settings/window_settings/window_settings.h
+++ src/settings/window_settings/window_settings.h
@@ -6,19 +6,21 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 13:47:19 by akovtune          #+#    #+#             */
-/*   Updated: 2025/06/02 12:41:04 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/03 17:59:05 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WINDOW_SETTINGS_H
 # define WINDOW_SETTINGS_H
 
+# define WINDOW_TITLE "cub3d"
+
 // Codam PC setup
-// # define WINDOW_WIDTH 1920
-// # define WINDOW_HEIGHT 1080
+# define WINDOW_WIDTH 1920
+# define WINDOW_HEIGHT 1080
 
 // My home PC setup
-# define WINDOW_WIDTH 960
-# define WINDOW_HEIGHT 540
+// # define WINDOW_WIDTH 960
+// # define WINDOW_HEIGHT 540
 
 #endif
