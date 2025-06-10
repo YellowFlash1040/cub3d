bbe3dd44636f728b469412536632aac1ca09bf44
YellowFlash1040 <andrejkovtunec@gmail.com>
2025-06-10 21:03:14 +0200

    Moved "pixel" class from canvas/ folder into gui/ folder Crateed new function: t_color	get_pixel_color(t_pixel	*pixel)

--- src/model/gui/pixel/pixel.h
+++ src/model/gui/pixel/pixel.h
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 16:54:43 by akovtune          #+#    #+#             */
-/*   Updated: 2025/05/24 19:51:02 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/10 19:22:19 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 # define PIXEL_H
 
 # include <bits/types.h>
+# include "color.h"
 
 typedef __uint8_t	t_pixel;
+
+t_color	get_pixel_color(t_pixel	*pixel);
 
 #endif
