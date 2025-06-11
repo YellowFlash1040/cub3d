095d342e6291be3177d6fab60c78ac4b6e9eb662
akovtune <akovtune@student.codam.nl>
2025-06-11 13:20:07 +0200

    Created enum animation_name inside animations.h

--- src/model/gui/animations/animations.h
+++ src/model/gui/animations/animations.h
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 16:21:01 by akovtune          #+#    #+#             */
-/*   Updated: 2025/06/10 17:07:54 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/11 13:19:18 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,18 @@
 # include "animation.h"
 
 # define ANIMATIONS_INIT_ERR 1
+
+typedef enum e_animation_name
+{
+	ANIMATION_NORTH_WALL,
+	ANIMATION_SOUTH_WALL,
+	ANIMATION_WEST_WALL,
+	ANIMATION_EAST_WALL,
+	ANIMATION_SPRITE,
+	ANIMATION_NAMES_COUNT
+}	t_animation_name;
+// ANIMATION_NAMES_COUNT is a nice trick, with which you 
+// can always tell how many items there are
 
 typedef struct animations
 {
