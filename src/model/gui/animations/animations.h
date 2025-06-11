2ef3fcebd774607683816c0597f8cfdc6f3ff90a
akovtune <akovtune@student.codam.nl>
2025-06-11 12:14:09 +0200

    Created "animations" class

---
+++ src/model/gui/animations/animations.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   animations.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 16:21:01 by akovtune          #+#    #+#             */
+/*   Updated: 2025/06/10 17:07:54 by akovtune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMATIONS_H
+# define ANIMATIONS_H
+
+# include <stdlib.h>
+# include "errors.h"
+# include "animation.h"
+
+# define ANIMATIONS_INIT_ERR 1
+
+typedef struct animations
+{
+	t_animation	*north_wall;
+	t_animation	*south_wall;
+	t_animation	*west_wall;
+	t_animation	*east_wall;
+	t_animation	*sprite;
+}	t_animations;
+
+t_animations	*init_animations(void);
+int				build_animations(t_animations **animations_ref);
+void			destroy_animations(t_animations **animations_ref);
+
+#endif
