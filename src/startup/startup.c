00803d37d2e39a401ca9cc7c0904e68c2f6a9319
YellowFlash1040 <andrejkovtunec@gmail.com>
2025-06-13 14:45:50 +0200

    Refactored code inside startup.c, by making use of the app_builder, config and game_setup

--- src/startup/startup.c
+++ src/startup/startup.c
@@ -6,29 +6,33 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 14:05:54 by akovtune          #+#    #+#             */
-/*   Updated: 2025/06/11 12:59:34 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/11 17:48:59 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "startup.h"
 
-static int	initialize_data(t_app *app);
 static int	build_data(t_app *app);
-static int	setup_data(t_app *app, t_settings *settings, t_map *map);
+static int	setup_data(t_app *app, t_configuration *config);
 
-int	startup(t_app *app, t_map *map)
+int	startup(t_app **app_ref, t_configuration *config)
 {
 	int			result;
-	t_settings	*settings;
+	t_app		*app;
 
-	settings = app->settings;
-	result = initialize_data(app);
+	if (!app_ref || !config)
+		return (EMPTY_PTR_ERR);
+	app = init_app();
+	if (!app)
+		return (MALLOC_FAIL_ERR);
+	*app_ref = app;
+	result = build_app(app_ref);
 	if (result != SUCCESS)
 		return (result);
 	result = build_data(app);
 	if (result != SUCCESS)
 		return (result);
-	result = setup_data(app, settings, map);
+	result = setup_data(app, config);
 	if (result != SUCCESS)
 		return (result);
 	result = canvas_to_window(app->mlx, app->canvas);
@@ -37,33 +41,13 @@ int	startup(t_app *app, t_map *map)
 	return (SUCCESS);
 }
 
-static int	initialize_data(t_app *app)
-{
-	app->mlx = mlx_init(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, false);
-	if (!app->mlx)
-		return (MLX_ERR);
-	app->canvas = init_canvas((t_size){WINDOW_WIDTH, WINDOW_HEIGHT});
-	if (!app->canvas)
-		return (MALLOC_FAIL_ERR);
-	app->textures = init_textures();
-	if (!app->textures)
-		return (MALLOC_FAIL_ERR);
-	app->animations = init_animations();
-	if (!app->animations)
-		return (MALLOC_FAIL_ERR);
-	app->game = init_game();
-	if (!app->game)
-		return (MALLOC_FAIL_ERR);
-	app->game->player = init_player();
-	if (!app->game->player)
-		return (MALLOC_FAIL_ERR);
-	return (SUCCESS);
-}
-
 static int	build_data(t_app *app)
 {
 	int	result;
 
+	result = build_game(&app->game);
+	if (result != SUCCESS)
+		return (result);
 	result = build_canvas(&app->canvas, app->mlx);
 	if (result != SUCCESS)
 		return (result);
@@ -79,17 +63,17 @@ static int	build_data(t_app *app)
 	return (SUCCESS);
 }
 
-static int	setup_data(t_app *app, t_settings *settings, t_map *map)
+static int	setup_data(t_app *app, t_configuration *config)
 {
 	int	result;
 
-	app->game->map = map;
-	setup_player(app->game->player, settings);
-	setup_camera(app->game->player->camera, settings);
-	result = setup_textures(app->textures, settings);
+	setup_game(app->game, config);
+	setup_player(app->game->player, config);
+	setup_camera(app->game->player->camera, app->settings, config);
+	result = setup_textures(app->textures, config);
 	if (result != SUCCESS)
 		return (result);
-	result = setup_animations(app->animations, settings);
+	result = setup_animations(app->animations, config);
 	if (result != SUCCESS)
 		return (result);
 	return (SUCCESS);
--- src/startup/startup.h
+++ src/startup/startup.h
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 14:06:08 by akovtune          #+#    #+#             */
-/*   Updated: 2025/06/11 12:51:33 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/11 17:48:20 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,14 @@
 # include "canvas.h"
 # include "renderer.h"
 # include "event_handlers.h"
+# include "configuration.h"
 
-# include "settings_setup.h"
+# include "game_setup.h"
 # include "player_setup.h"
 # include "camera_setup.h"
 # include "textures_setup.h"
 # include "animations_setup.h"
 
-int	startup(t_app *app, t_map *map);
+int	startup(t_app **app_ref, t_configuration *config);
 
 #endif
