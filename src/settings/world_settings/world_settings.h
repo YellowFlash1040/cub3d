0af5740da08c46e06adb911f50591fc93775b62d
akovtune <akovtune@student.codam.nl>
2025-06-15 18:51:58 +0200

    Updated world_settings.h

--- src/settings/world_settings/world_settings.h
+++ src/settings/world_settings/world_settings.h
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 13:48:41 by akovtune          #+#    #+#             */
-/*   Updated: 2025/06/02 12:45:34 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/14 14:11:40 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 # define WORLD_SETTINGS_H
 
 // Codam PC setup
-// # define CELL_SIZE 64
+# define CELL_SIZE 64
+# define SPRITE_SIZE 32
 
 // My home PC setup
-# define CELL_SIZE 32
+// # define CELL_SIZE 32
 
 #endif
