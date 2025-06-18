a86325549d86ef14b96df80280730d60464d2114
akovtune <akovtune@student.codam.nl>
2025-06-18 17:37:41 +0200

    Added clamp_bounds() function inside draw_rectangle() function inside drawing.c

--- src/logic/gui/drawing/drawing.c
+++ src/logic/gui/drawing/drawing.c
@@ -6,35 +6,50 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 16:22:57 by akovtune          #+#    #+#             */
-/*   Updated: 2025/06/02 20:55:00 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/18 17:26:29 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "drawing.h"
 
 static int	determine_step(int start, int end);
+static void	clamp_bounds(t_size canvas_size, t_point *top_left,
+				t_point *bottom_right);
 
 void	draw_rectangle(t_canvas *canvas, t_point top_left, t_size size,
 	t_color color)
 {
-	int	x;
-	int	y;
-	int	x_end;
-	int	y_end;
+	int		x;
+	int		y;
+	t_point	end;
 
-	x_end = top_left.x + size.width;
-	y_end = top_left.y + size.height;
+	end.x = top_left.x + size.width;
+	end.y = top_left.y + size.height;
+	clamp_bounds(canvas->size, &top_left, &end);
 	y = top_left.y;
-	while (y < y_end)
+	while (y < end.y)
 	{
 		x = top_left.x;
-		while (x < x_end)
+		while (x < end.x)
 		{
 			mlx_put_pixel(canvas->image, x, y, color);
 			x++;
 		}
 		y++;
 	}
+}
+
+static void	clamp_bounds(t_size canvas_size, t_point *top_left,
+	t_point *bottom_right)
+{
+	if (top_left->x < 0)
+		top_left->x = 0;
+	if (top_left->y < 0)
+		top_left->y = 0;
+	if (bottom_right->x > canvas_size.width)
+		bottom_right->x = canvas_size.width;
+	if (bottom_right->y > canvas_size.height)
+		bottom_right->y = canvas_size.height;
 }
 
 // Bresenham's Line Algorithm
