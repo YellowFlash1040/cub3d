13b182a82c3017d1b7d12af58d5738aa9f4d13d5
akovtune <akovtune@student.codam.nl>
2025-06-18 14:24:13 +0200

    Sprites - step 3
    I have 3 versions of the function draw_sprite here:
    - AI Function
    - Function, which is 80% mine, and works fine
    - My function, which is not perfect
    
    And I figured out how the function that is 80% mine
    works. So I want to save, before improving the readability
    and adding textures on top of it

--- src/logic/gui/scene/sprites_drawing/sprites_drawing.h
+++ src/logic/gui/scene/sprites_drawing/sprites_drawing.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sprites.h                                          :+:      :+:    :+:   */
+/*   sprites_drawing.h                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 14:09:31 by akovtune          #+#    #+#             */
-/*   Updated: 2025/06/13 17:13:14 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/17 20:39:44 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SPRITES_H
-# define SPRITES_H
+#ifndef SPRITES_DRAWING_H
+# define SPRITES_DRAWING_H
 
 # include "settings.h"
 # include "canvas.h"
@@ -22,6 +22,8 @@
 # include "geometry.h"
 
 # include <stdio.h>
+
+# define PROJECTION_PLANE_WIDTH 2
 
 void	draw_sprites(t_canvas *canvas, t_camera *camera, t_game *game,
 			t_textures *textures);
