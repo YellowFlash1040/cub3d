2b8575d3307ab11d085730620ae39ddc509d9350
akovtune <akovtune@student.codam.nl>
2025-06-21 12:43:38 +0200

    Replaced SPRITE_SIZE with SPRITE_WIDTH and SPRITE_HEIGHT inside world_settings.h

--- src/settings/world_settings/world_settings.h
+++ src/settings/world_settings/world_settings.h
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 13:48:41 by akovtune          #+#    #+#             */
-/*   Updated: 2025/06/18 16:38:17 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/19 19:33:47 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 // Codam PC setup
 # define CELL_SIZE 64
-# define SPRITE_SIZE 64
+# define SPRITE_WIDTH 64
+# define SPRITE_HEIGHT 64
 
 // My home PC setup
 // # define CELL_SIZE 32
