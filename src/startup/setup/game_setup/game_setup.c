bb2e440d944f7ccb17b400b45d59fe1f7589993c
akovtune <akovtune@student.codam.nl>
2025-06-21 15:32:07 +0200

    Fixed possible memory leak

--- src/startup/setup/game_setup/game_setup.c
+++ src/startup/setup/game_setup/game_setup.c
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 17:38:20 by akovtune          #+#    #+#             */
-/*   Updated: 2025/06/11 17:49:05 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/21 15:16:07 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,6 @@ void	setup_game(t_game *game, t_configuration *config)
 	game->map = config->map;
 	game->npcs = config->npcs;
 	game->npc_count = config->npc_count;
+	config->map = NULL;
+	config->npcs = NULL;
 }
