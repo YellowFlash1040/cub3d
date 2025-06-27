c59996663139bc7829f69c3a3bcd67c589be2aa1
akovtune <akovtune@student.codam.nl>
2025-06-27 18:37:40 +0200

    Fixed segfault caused by the is_door() function

--- src/model/core/map/map_methods.c
+++ src/model/core/map/map_methods.c
@@ -6,7 +6,7 @@
 /*   By: akovtune <akovtune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 17:17:27 by akovtune          #+#    #+#             */
-/*   Updated: 2025/06/04 17:17:36 by akovtune         ###   ########.fr       */
+/*   Updated: 2025/06/27 18:04:15 by akovtune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ bool	is_obstacle(t_map *map, int x, int y)
 
 bool	is_door(t_map *map, int x, int y)
 {
-	if (x < 0 || x > map->width)
+	if (x < 0 || x >= map->width)
 		return (false);
-	if (y < 0 || y > map->height)
+	if (y < 0 || y >= map->height)
 		return (false);
 	if (map->cells[y][x] == CLOSED_DOOR)
 		return (true);
